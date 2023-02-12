69a35bdbf7080ac6fbad43393d0509651bc64b73
Surendra Anand <anand3294s@gmail.com>
2023-02-12 11:46:38 +0530

    12-Feb Changes AM

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.sql.*;
+
 public class Main {
+
+    private static String dbUser, dbPassword, dbURL, sqlQuery;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        dbUser = "root";
+        dbPassword = "3Fugitive!";
+        dbURL = "jdbc:mysql://localhost:3306/classicmodels";
+
+        sqlQuery = "SELECT * FROM orders where orderNumber in (10104,10106,10108)";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(dbURL, dbUser, dbPassword);
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+            System.out.println("Established connection successfully...");
+            
+            ResultSetMetaData rsmd = resultSet.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+
+            while (resultSet.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1)
+                        System.out.print(",  ");
+
+                    System.out.print(resultSet.getString(i));
+                }
+                System.out.println();
+            }
+
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
+
 }
